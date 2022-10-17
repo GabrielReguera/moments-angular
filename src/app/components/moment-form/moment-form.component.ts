690a564df33c8ba30ef48e900ad5841683d1c7c1
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-17 11:18:40 -0300

    Creating interfaces and configuring modules

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,16 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
-  @Input() btnText!: string
+  @Input() btnText!: string;
 
-  constructor() { }
+  imageURL!: string;
+  uploadForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(public fb: FormBuilder) {
+    this.uploadForm = this.fb.group({
+      avatar: [null],
+      name: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  showPreview(event: any) {
+    const file = (event.target as HTMLInputElement).files![0];
+    this.uploadForm.patchValue({
+      avatar: file
+    });
+    this.uploadForm.get('avatar')?.updateValueAndValidity()
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageURL = reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
 }
